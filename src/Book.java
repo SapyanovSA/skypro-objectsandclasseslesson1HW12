853bb9153a8adbf3f37970abb24b34db408afba2
helloworld(1) <sapanovs04@gmail.com>
2026-04-20 20:33:50 +0300

    Homework-12 completion

---
+++ src/Book.java
@@ -0,0 +1,31 @@
+public class Book {
+
+    private String nameBook;
+    private Author author;
+    private int yearPublication;
+
+    public Book(String nameBook, Author author, int yearPublication) {
+        this.nameBook = nameBook;
+        this.author = author;
+        this.yearPublication = yearPublication;
+    }
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getYearPublication() {
+        return yearPublication;
+    }
+
+    public void setYearPublication(int yearPublication) {
+        this.yearPublication = yearPublication;
+    }
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
+
+        Author author_1 = new Author("Сергей", "Пушкин");
+        Author author_2 = new Author("Лев", "Толстой");
+
+        Book book_1 = new Book("Капитанская дочка", author_1, 1836);
+        Book book_2 = new Book("Войнна и мир", author_2, 1863);
+
+        System.out.println("Название: " + book_1.getNameBook() + " Автор: " + book_1.getAuthor().getName() + " " + book_1.getAuthor().getSurname() + " Год издания: " + book_1.getYearPublication());
+        System.out.println("Название: " + book_2.getNameBook() + " Автор: " + book_2.getAuthor().getName() + " " + book_2.getAuthor().getSurname() + " Год издания: " + book_2.getYearPublication());
+
+        book_2.setYearPublication(1869);
+        System.out.println("Название: " + book_2.getNameBook() + " Автор: " + book_2.getAuthor().getName() + " " + book_2.getAuthor().getSurname() + " Год издания: " + book_2.getYearPublication());
 
     }
 }
