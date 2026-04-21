c656d2107db6612955de2acc108da857222a227a
helloworld(1) <sapanovs04@gmail.com>
2026-04-21 14:41:35 +0300

    Homework-13

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
 
     private String nameBook;
@@ -26,6 +28,22 @@ public class Book {
         this.yearPublication = yearPublication;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameBook, author, yearPublication);
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        Book bookCurrent = (Book) o;
+        return Objects.equals(nameBook, bookCurrent.nameBook) && Objects.equals(author, bookCurrent.author) && Objects.equals(yearPublication, bookCurrent.yearPublication);
+    }
+
+    @Override
+    public String toString() {
+        return "Название книги: " + nameBook + ", Автор: " + author.toString() + ", Год издания: " + yearPublication;
+    }
 
 }
